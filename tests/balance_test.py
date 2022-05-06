9330e07f0216f6bd5a88e5cbf0ddd33f0d6dee23
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-05-06 12:05:27 -0400

    update blalance test

---
+++ tests/balance_test.py
@@ -0,0 +1,34 @@
+import logging
+import os
+
+from app import db
+from app.db.models import User, Transaction
+
+def test_balance(application, client):
+
+    with application.app_context():
+        user = {'email': '123@gmail.com','password': '123456'}
+        client.post("/register", data=user)
+
+        user_2 ={'email': '123@gmail.com','password': '123456'}
+
+        client.post("/login",data = user_2)
+        root = os.path.dirname(os.path.abspath(__file__))
+        testdir = os.path.join(root, '../tests')
+        assert os.path.exists(testdir) == True
+
+        test_file = os.path.join(testdir, 't4.csv')
+        assert os.path.exists(test_file) == True
+        upload_dir = os.path.join(root, '../app/uploads')
+        assert os.path.exists(upload_dir)
+
+        test_file_2 ={
+            'file' : open(test_file,'rb')
+        }
+        responce = client.post('/app/uploads', data = test_file_2)
+        assert responce.status_code == 404
+        #assert user.balance == 200.00
+
+        assert len(os.listdir(upload_dir)) == 0
+        for f in os.listdir(upload_dir):
+            os.remove(os.path.join(upload_dir,f))
