10e32bef10b2f56f2a702576d7b901c542407ec2
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-05-06 11:45:27 -0400

    update upload test

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -1,25 +1,32 @@
+import logging
 import os
+
+from app import db
+from app.db.models import User, Transaction
 
 def test_upload_file(application, client):
 
     with application.app_context():
-        data = {
-            'email': '123@gmail.com',
-            'password': '123456',
-            'confirm': '123456'
-        }
-        client.post("/register", data=data)
+        user = {'email': '123@gmail.com','password': '123456'}
+        client.post("/register", data=user)
 
-        data2 ={
-            'email': '123@gmail.com',
-            'password': '123456'
+        user_2 ={'email': '123@gmail.com','password': '123456'}
 
-        }
-
-        client.post("/login",data = data2)
+        client.post("/login",data = user_2)
         root = os.path.dirname(os.path.abspath(__file__))
         testdir = os.path.join(root, '../tests')
         assert os.path.exists(testdir) == True
 
-        test_file = os.path.join(testdir, 'test.csv')
+        test_file = os.path.join(testdir, 't4.csv')
         assert os.path.exists(test_file) == True
+        upload_dir = os.path.join(root, '../app/uploads')
+        assert os.path.exists(upload_dir)
+
+        test_file_2 ={
+            'file' : open(test_file,'rb')
+        }
+        responce = client.post('/app/uploads', data = test_file_2)
+        assert responce.status_code == 404
+        assert len(os.listdir(upload_dir)) == 0
+        for f in os.listdir(upload_dir):
+            os.remove(os.path.join(upload_dir,f))
